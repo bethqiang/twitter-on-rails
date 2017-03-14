c0b8596dc919dfecb2d36def5e1ce9a559afed59
Beth Qiang <bethany.qiang@gmail.com>
2017-03-14 14:32:40 -0500

    Add routes into links & tests for routes/links

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    # a[href=?] allows us to test for the presence of a particular link-URL combination
+    # Rails automatically inserts the value of root_path in place of the question mark
+    # Asserts that there are two such links (logo and nav element)
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    get contact_path
+    assert_select "title", full_title("Contact")
+  end
+end
