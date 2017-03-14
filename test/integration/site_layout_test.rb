6b0d9d905dadb4e7ee913189519eee761c3cb596
Beth Qiang <bethany.qiang@gmail.com>
2017-03-14 14:44:30 -0500

    Add first steps of user signup

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -11,7 +11,14 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+
+    get help_path
+    assert_select "title", full_title("Help")
+    get about_path
+    assert_select "title", full_title("About")
     get contact_path
     assert_select "title", full_title("Contact")
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
 end
