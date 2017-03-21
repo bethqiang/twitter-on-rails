9ef9226a0928d1e2cf891a381a8ea0cb0ba1f082
Beth Qiang <bethany.qiang@gmail.com>
2017-03-20 21:37:43 -0500

    Add signup flash & valid signup test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save
+      flash[:success] = "Welcome to Twitter on Rails!"
+      redirect_to @user
     else
       render 'new'
     end
